49edcf04175f6d669b44f44d6ab60ed903d0e93f
Lluc Llull <llull99.80@gmail.com>
2025-06-28 20:46:32 +0200

    feat: refactor app structure and add layout component
    - Removed placeholder content from app.component.html and replaced it with <app-layout> component.
    - Deleted app.component.spec.ts as it is no longer needed.
    - Updated app.component.ts to import and use LayoutComponent.
    - Enhanced app.config.ts to include HTTP client support.
    - Created layout.component.html and layout.component.ts for layout structure.
    - Added ApiService for handling API requests with Supabase.
    - Removed SupabaseService as it is redundant with the new ApiService.
    - Updated environment.ts with new API base URL and keys.

---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LayoutComponent {
+}
---
+++ src/app/services/api/api.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private headers = new HttpHeaders({
+    apikey: environment.supabaseAnonKey,
+    Authorization: `Bearer ${environment.supabaseAnonKey}`,
+    Accept: 'application/json',
+  });
+
+  constructor(private http: HttpClient) {}
+
+  getGeneralConfig(): Observable<GeneralConfigResponse[]> {
+    return this.http.get<GeneralConfigResponse[]>(
+      `${environment.apiBaseUrl}/config`,
+      { headers: this.headers }
+    );
+  }
+}
+
+export interface GeneralConfigResponse {
+  id: number;
+  name: string;
+  favicons: string | null;
+  languages: {
+    id: number;
+    code: string;
+  }[];
+  logos: string | null;
+  pages: Page[];
+  theme: string;
+}
+
+export interface Page {
+  id: number;
+  name: string;
+  template: string;
+}
