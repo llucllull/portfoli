72e82bc1efeac9fdc009098c92dd6b99fa10f7c8
Lluc Llull <llull99.80@gmail.com>
2025-07-10 17:35:15 +0200

    feat: implement OnInit lifecycle for AppComponent and initialize SiteConfigService and RoutesService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LayoutComponent } from "./layout/layout.component";
+import { SiteConfigService } from './services/site-config/site-config.service';
+import { RoutesService } from './services/routes/routes.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, LayoutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfoli';
+
+  constructor(
+    private siteConfig: SiteConfigService,
+    private routesService: RoutesService
+  ) {}
+
+  ngOnInit() {
+    this.siteConfig.init().subscribe(() => {
+      this.routesService.init().subscribe(() => {
+        // Ja tens idiomes i rutes carregades
+      });
+    });
+  }
 }
