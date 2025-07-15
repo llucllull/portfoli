a05378fe3bbc22ea8fd26958f27fc06d2e75d7de
Lluc Llull <llull99.80@gmail.com>
2025-07-15 18:53:36 +0200

    feat: add header component to layout

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HeaderClearComponent } from '@lluc_llull/ui-lib';
+
+const COMPONENTS = [
+  HeaderClearComponent,
+];
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ...COMPONENTS],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LayoutComponent {
+  
 }
