f2008b7ea127a8ed5dd0d4cefa29c0d6d405dbba
Lluc Llull <llull99.80@gmail.com>
2025-07-15 18:52:55 +0200

    feat: define getters and setters for layout service

--- src/app/services/layout/layout.service.ts
+++ src/app/services/layout/layout.service.ts
@@ -1,6 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { BodyComponent } from '@lluc_llull/ui-lib';
+
+export type HeaderComponentName = 'headerClear';
 
 @Injectable({
   providedIn: 'root',
 })
-export class LayoutService {}
+export class LayoutService {
+  private readonly header = signal<BodyComponent<any> | undefined>(undefined);
+  private readonly layoutLoaded = signal<boolean>(false);
+
+  // Getters
+  get headerComponent(): BodyComponent<any> | undefined {
+    return this.header();
+  }
+
+  get layoutReady(): boolean {
+    return this.layoutLoaded();
+  }
+
+  // Setters
+  setHeader(component: BodyComponent<any>) {
+    this.header.set(component);
+  }
+
+  markLayoutAsLoaded() {
+    this.layoutLoaded.set(true);
+  }
+
+  resetLayout() {
+    this.header.set(undefined);
+    this.layoutLoaded.set(false);
+  }
+}
