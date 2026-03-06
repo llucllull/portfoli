c58da4c05a43e7c320d9cf117a11126334fe5b0f
Lluc Llull <lluc@clicktotravel.es>
2026-03-06 12:01:26 +0100

    feat: add new getters from cdn to content service

--- src/app/services/content/content.service.ts
+++ src/app/services/content/content.service.ts
@@ -3,24 +3,38 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContentService {
-
   private base = environment.contentBaseUrl;
 
   constructor(private http: HttpClient) {}
 
-  getSite() {
-    return this.http.get(`${this.base}/site.json`);
+  getConfig() {
+    return this.http.get(`${this.base}/config.json`);
+  }
+
+  getLanguages() {
+    return this.http.get(`${this.base}/languages.json`);
+  }
+
+  getNavigation() {
+    return this.http.get(`${this.base}/navigation.json`);
+  }
+
+  getSocial() {
+    return this.http.get(`${this.base}/social.json`);
+  }
+
+  getPage(page: string) {
+    return this.http.get(`${this.base}/pages/${page}.json`);
   }
 
   getProjects() {
-    return this.http.get(`${this.base}/projects.json`);
+    return this.http.get(`${this.base}/projects/projects.json`);
   }
 
-  getProject(slug: string) {
-    return this.http.get(`${this.base}/projects/${slug}.json`);
+  getProject(project: string) {
+    return this.http.get(`${this.base}/projects/${project}.json`);
   }
-
 }
