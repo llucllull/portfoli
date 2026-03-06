df1d6e4448be1b68b5e8cfe18ca92dc53f5aff1e
Lluc Llull <lluc@clicktotravel.es>
2026-03-06 13:28:16 +0100

    refactor: app.component, app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,34 +1,18 @@
-import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { RoutesService } from './services/routes/routes.service';
-import { MapperService } from '@lluc_llull/ui-lib';
-import { SiteConfigService } from './services/site-config/site-config.service';
-import { firstValueFrom } from 'rxjs';
+import { ApplicationConfig } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
+
+import { MapperService } from '@lluc_llull/ui-lib';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter([]),
-    {
-      provide: APP_INITIALIZER,
-      useFactory: appInitializer,
-      deps: [SiteConfigService, RoutesService],
-      multi: true,
-    },
+    provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(withFetch()),
-    MapperService, provideAnimationsAsync(),
+    provideAnimationsAsync(),
+    MapperService,
   ],
 };
-
-export function appInitializer(
-  siteConfig: SiteConfigService,
-  routes: RoutesService
-) {
-  return async () => {
-    await firstValueFrom(siteConfig.init());
-    await firstValueFrom(routes.init());
-  };
-}
