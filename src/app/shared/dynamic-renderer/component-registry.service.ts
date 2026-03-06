8777ad26fb565e4aae1325dceb5b659ebc749bbf
Lluc Llull <lluc@clicktotravel.es>
2026-03-06 13:28:55 +0100

    feat: add dynamic renderer, add component registry

---
+++ src/app/shared/dynamic-renderer/component-registry.service.ts
@@ -0,0 +1,18 @@
+import { Type } from '@angular/core';
+
+export const COMPONENT_REGISTRY: Record<string, () => Promise<Type<any>>> = {
+  'header-clear': () =>
+    import('@lluc_llull/ui-lib').then((m) => m.HeaderClearComponent),
+
+  'header-mobile': () =>
+    import('@lluc_llull/ui-lib').then((m) => m.HeaderMobileComponent),
+
+  'hero-section': () =>
+    import('@lluc_llull/ui-lib').then((m) => m.HeroSectionComponent),
+
+  'section-intro': () =>
+    import('@lluc_llull/ui-lib').then((m) => m.SectionIntroComponent),
+
+  'category-progress': () =>
+    import('@lluc_llull/ui-lib').then((m) => m.CategoryProgressComponent),
+};
---
+++ src/app/shared/dynamic-renderer/dynamic-renderer.component.ts
@@ -0,0 +1,47 @@
+import {
+  Component,
+  inject,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ViewContainerRef,
+} from '@angular/core';
+
+import { BodyComponent } from '@lluc_llull/ui-lib';
+import { COMPONENT_REGISTRY } from './component-registry.service';
+
+@Component({
+  selector: 'dynamic-renderer',
+  standalone: true,
+  template: '',
+})
+export class DynamicRendererComponent implements OnChanges {
+  @Input() components: BodyComponent<any>[] = [];
+
+  private vcr = inject(ViewContainerRef);
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (!changes['components'] || !this.components?.length) return;
+
+    this.render();
+  }
+
+  private async render() {
+    this.vcr.clear();
+
+    for (const c of this.components) {
+      const loader = COMPONENT_REGISTRY[c.name];
+
+      if (!loader) {
+        console.warn(`Component "${c.name}" not registered`);
+        continue;
+      }
+
+      const component = await loader();
+
+      const ref = this.vcr.createComponent(component);
+
+      Object.assign(ref.instance as any, c.props);
+    }
+  }
+}
