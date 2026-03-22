598f9e5b8e5edd4018f37b65529ac659e92cb350
Lluc Llull <llull99.80@gmail.com>
2026-03-22 19:58:26 +0100

    fix: handling 404 on vercel

--- src/app/resolvers/page.resolver.ts
+++ src/app/resolvers/page.resolver.ts
@@ -7,11 +7,14 @@ export class pageResolver implements Resolve<boolean> {
   constructor(private store: ContentStore) {}
 
   async resolve(route: any): Promise<boolean> {
-    const slug =
-      route.paramMap.get('slug') || route.routeConfig?.path || 'home';
+    let slug = route.paramMap.get('slug') || route.routeConfig?.path || 'home';
+
+    // Si Angular cayó en la ruta comodín, forzamos el slug a '404'
+    if (slug === '**') {
+      slug = '404';
+    }
 
     await this.store.loadPage(slug);
-
     return true;
   }
 }
