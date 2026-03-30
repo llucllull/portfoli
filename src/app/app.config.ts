ca1c34038b48790a961640f90f091e172a5dda15
Lluc Llull <llull99.80@gmail.com>
2026-03-30 19:01:58 +0200

    fix: reorganize imports and enhance provideClientHydration with withEventReplay for improved clarity

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,26 +1,36 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom  } from '@angular/core';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
+import {
+  PreloadAllModules,
+  provideRouter,
+  withPreloading,
+} from '@angular/router';
 import { ArrowRight, ArrowUpRight, LucideAngularModule } from 'lucide-angular';
 
-import { CDN_BASE_URL, MapperService } from '@lluc_llull/ui-lib/mapper';
-import { routes } from './app.routes';
-import { environment } from '../environments/environment';
 import { MatDialogModule } from '@angular/material/dialog';
+import { CDN_BASE_URL, MapperService } from '@lluc_llull/ui-lib/mapper';
+import { environment } from '../environments/environment';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient(withFetch()),
     provideAnimationsAsync(),
-    provideClientHydration(),
-    importProvidersFrom(MatDialogModule, LucideAngularModule.pick({ArrowRight, ArrowUpRight})),
+    importProvidersFrom(
+      MatDialogModule,
+      LucideAngularModule.pick({ ArrowRight, ArrowUpRight }),
+    ),
     MapperService,
     {
       provide: CDN_BASE_URL,
-      useValue: environment.assetsBaseUrl
+      useValue: environment.assetsBaseUrl,
     },
+    provideClientHydration(withEventReplay()),
   ],
 };
