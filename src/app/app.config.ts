4325a0d89da25bc5a827cb894e82e99714eefee3
Lluc Llull <llull99.80@gmail.com>
2026-04-21 19:37:24 +0200

    refactor: clean up imports and remove unused app initializer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,8 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom, provideAppInitializer } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom
+} from '@angular/core';
 import {
   provideClientHydration,
   withEventReplay,
@@ -18,11 +21,9 @@ import { CDN_BASE_URL, MapperService } from '@lluc_llull/ui-lib/mapper';
 import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 import { SeoService } from './services/seo/seo.service';
-import { preloadComponents } from './shared/dynamic-renderer/component-registry.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideAppInitializer(() => preloadComponents()), 
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient(withFetch()),
     provideAnimationsAsync(),
