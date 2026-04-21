85a236f0c749983efe6f77eda3bb2fc8c392db47
Lluc Llull <llull99.80@gmail.com>
2026-04-21 19:39:02 +0200

    feat: undo last commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,5 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import {
-  ApplicationConfig,
-  importProvidersFrom
-} from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideAppInitializer } from '@angular/core';
 import {
   provideClientHydration,
   withEventReplay,
@@ -21,9 +18,11 @@ import { CDN_BASE_URL, MapperService } from '@lluc_llull/ui-lib/mapper';
 import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 import { SeoService } from './services/seo/seo.service';
+import { preloadComponents } from './shared/dynamic-renderer/component-registry.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAppInitializer(() => preloadComponents()), 
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideHttpClient(withFetch()),
     provideAnimationsAsync(),
